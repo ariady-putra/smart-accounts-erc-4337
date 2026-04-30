fa3aa73a0120d6566004775ddd0f227bb3aa67a8
Ariady Putra <ariady.putra@gmail.com>
2026-04-30 21:03:51 +0700

    Part 4

--- sepolia.js
+++ sepolia.js
@@ -27,12 +27,6 @@ import { ACCOUNT, ACCOUNT_FACTORY, ENTRY_POINT, PAYMASTER, SEPOLIA } from "./con
     args: [userAccountAddress, 0],
   });
 
-  const callGasLimit = 800_000n;
-  const verificationGasLimit = 800_000n;
-  const postOpGasLimit = 800_000n; // for Paymaster's postOp gas limit
-  const maxFeePerGas = parseGwei("40");
-  const maxPriorityFeePerGas = parseGwei("20");
-
   const userOp = {
     sender: userAccountAddress,
     nonce: toHex(nonce),
@@ -41,28 +35,51 @@ import { ACCOUNT, ACCOUNT_FACTORY, ENTRY_POINT, PAYMASTER, SEPOLIA } from "./con
     paymasterAndData: PAYMASTER.sepolia.address,
     signature: "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c",
   };
-  const gasEstimation = await SEPOLIA.request({
+
+  // TODO: Everything onward hasn't been tested successfully. Waiting for Alchemy EntryPoint v0.9.0 support
+
+  const { preVerificationGas, verificationGasLimit, callGasLimit } = await SEPOLIA.request({
     method: "eth_estimateUserOperationGas",
     params: [userOp, ENTRY_POINT.sepolia.address],
   });
-  console.log({ gasEstimation });
+  userOp.paymasterAndData = encodePacked(["address", "uint128", "uint128"], [PAYMASTER.foundry.address, verificationGasLimit, 800_000n]);
+  userOp.accountGasLimits = encodePacked(["uint128", "uint128"], [verificationGasLimit, callGasLimit]);
+  userOp.preVerificationGas = toHex(preVerificationGas);
 
-  // userOp.signature = await SEPOLIA.signMessage({ message: { raw: userOpHash } });
+  const { maxFeePerGas } = await SEPOLIA.estimateFeesPerGas();
+  const maxPriorityFeePerGas = await SEPOLIA.request({ method: "rundler_maxPriorityFeePerGas" });
+  userOp.gasFees = encodePacked(["uint128", "uint128"], [maxPriorityFeePerGas, maxFeePerGas]);
 
-  // const { request } = await SEPOLIA.simulateContract({
-  //   address: ENTRY_POINT.sepolia.address,
-  //   abi: ENTRY_POINT.abi,
-  //   functionName: "handleOps",
-  //   args: [[userOp], SEPOLIA.account.address],
-  // });
-  // const hash = await SEPOLIA.writeContract(request);
-  // const { status } = await SEPOLIA.waitForTransactionReceipt({ hash });
-  // console.log({ status });
-  // console.log({
-  //   number: await SEPOLIA.readContract({
-  //     address: userAccountAddress,
-  //     abi: ACCOUNT.abi,
-  //     functionName: "number",
-  //   })
-  // });
+  const userOpHash = await FOUNDRY.readContract({
+    address: ENTRY_POINT.foundry.address,
+    abi: ENTRY_POINT.abi,
+    functionName: "getUserOpHash",
+    args: [userOp],
+  });
+
+  userOp.signature = await SEPOLIA.signMessage({ message: { raw: userOpHash } });
+
+  const operationHash = await SEPOLIA.request({
+    method: "eth_sendUserOperation",
+    params: [userOp, ENTRY_POINT.sepolia.address],
+  });
+
+  const test = setTimeout(async () => {
+    const { transactionHash } = await SEPOLIA.request({
+      method: "eth_getUserOperationByHash",
+      params: [operationHash],
+    });
+
+    const { status } = await SEPOLIA.waitForTransactionReceipt({ hash: transactionHash });
+    console.log({ status });
+    console.log({
+      number: await SEPOLIA.readContract({
+        address: userAccountAddress,
+        abi: ACCOUNT.abi,
+        functionName: "number",
+      })
+    });
+
+    clearTimeout(test);
+  }, 12_000);
 })();

2a3f9bf96edf811ed191bfd7b9ce703fb54ebcfd
Ariady Putra <ariady.putra@gmail.com>
2026-05-02 13:05:56 +0700

    Prepare for EntryPoint v0.9.0 support

--- sepolia.js
+++ sepolia.js
@@ -20,35 +20,53 @@ import { ACCOUNT, ACCOUNT_FACTORY, ENTRY_POINT, PAYMASTER, SEPOLIA } from "./con
   });
   const isDeployed = fromHex(deployedAddress, "bigint") > 0n;
 
+  // https://www.alchemy.com/docs/wallets/api-reference/gas-manager-admin-api/gas-abstraction-api-endpoints/alchemy-request-gas-and-paymaster-and-data
+  const policyId = process.env.PM_POLICY_ID; // The Gas Manager Policy ID
+  const entryPoint = ENTRY_POINT.sepolia.address; // The EntryPoint address the request should be sent through
+  const dummySignature = "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c"; // Dummy signature for the userOperation
+
   const nonce = await SEPOLIA.readContract({
     address: ENTRY_POINT.sepolia.address,
     abi: ENTRY_POINT.abi,
     functionName: "getNonce",
     args: [userAccountAddress, 0],
   });
-
-  const userOp = {
+  const userOperation = {
     sender: userAccountAddress,
     nonce: toHex(nonce),
     initCode: isDeployed ? "0x" : `${ACCOUNT_FACTORY.sepolia.address}${create2Account.slice(2)}`,
     callData: `0x${ACCOUNT.methodIdentifiers["increment()"]}`,
-    paymasterAndData: PAYMASTER.sepolia.address,
-    signature: "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c",
+    signature: dummySignature,
   };
 
   // TODO: Everything onward hasn't been tested successfully. Waiting for Alchemy EntryPoint v0.9.0 support
-
-  const { preVerificationGas, verificationGasLimit, callGasLimit } = await SEPOLIA.request({
-    method: "eth_estimateUserOperationGas",
-    params: [userOp, ENTRY_POINT.sepolia.address],
+  const {
+    paymaster,
+    paymasterData,
+    callGasLimit,
+    verificationGasLimit,
+    preVerificationGas,
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    paymasterVerificationGasLimit,
+    paymasterPostOpGasLimit,
+  } = await SEPOLIA.request({
+    method: "alchemy_requestGasAndPaymasterAndData",
+    params: [{ policyId, entryPoint, dummySignature, userOperation }],
   });
-  userOp.paymasterAndData = encodePacked(["address", "uint128", "uint128"], [PAYMASTER.foundry.address, verificationGasLimit, 800_000n]);
-  userOp.accountGasLimits = encodePacked(["uint128", "uint128"], [verificationGasLimit, callGasLimit]);
-  userOp.preVerificationGas = toHex(preVerificationGas);
-
-  const { maxFeePerGas } = await SEPOLIA.estimateFeesPerGas();
-  const maxPriorityFeePerGas = await SEPOLIA.request({ method: "rundler_maxPriorityFeePerGas" });
-  userOp.gasFees = encodePacked(["uint128", "uint128"], [maxPriorityFeePerGas, maxFeePerGas]);
+  userOperation.paymasterAndData = `${paymaster}${encodePacked(
+    ["uint128", "uint128"],
+    [fromHex(paymasterVerificationGasLimit, "bigint"), fromHex(paymasterPostOpGasLimit, "bigint")],
+  ).slice(2)}${paymasterData.slice(2)}`;
+  userOperation.accountGasLimits = encodePacked(
+    ["uint128", "uint128"],
+    [fromHex(verificationGasLimit, "bigint"), fromHex(callGasLimit, "bigint")],
+  );
+  userOperation.gasFees = encodePacked(
+    ["uint128", "uint128"],
+    [fromHex(maxPriorityFeePerGas, "bigint"), fromHex(maxFeePerGas, "bigint")],
+  );
+  userOperation.preVerificationGas = preVerificationGas;
 
   const userOpHash = await FOUNDRY.readContract({
     address: ENTRY_POINT.foundry.address,
@@ -56,20 +74,17 @@ import { ACCOUNT, ACCOUNT_FACTORY, ENTRY_POINT, PAYMASTER, SEPOLIA } from "./con
     functionName: "getUserOpHash",
     args: [userOp],
   });
-
-  userOp.signature = await SEPOLIA.signMessage({ message: { raw: userOpHash } });
+  userOperation.signature = await SEPOLIA.signMessage({ message: { raw: userOpHash } });
 
   const operationHash = await SEPOLIA.request({
     method: "eth_sendUserOperation",
-    params: [userOp, ENTRY_POINT.sepolia.address],
+    params: [userOperation, ENTRY_POINT.sepolia.address],
   });
-
   setTimeout(async () => {
     const { transactionHash } = await SEPOLIA.request({
       method: "eth_getUserOperationByHash",
       params: [operationHash],
     });
-
     const { status } = await SEPOLIA.waitForTransactionReceipt({ hash: transactionHash });
     console.log({ status });
     console.log({
